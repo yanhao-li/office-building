1dc34a2d8c43b81100bcbdf407755151daade7b8
Yanhao Li <hi@yanhaoli.com>
2018-05-11 18:23:27 -0400

    updated ignore

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit8699ee7d59bc58a40f9c48511ad30832
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit8699ee7d59bc58a40f9c48511ad30832::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire8699ee7d59bc58a40f9c48511ad30832($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire8699ee7d59bc58a40f9c48511ad30832($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
